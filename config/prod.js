f66bcbc4cb487556f235232c036ed5ce6069d667
Leonardo Freire <lfreire80@gmail.com>
2018-11-06 17:49:42 -0300

    env keys

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+
+// module.exports = {
+//     googleClientID: '989937844917-93bb2f1pd5ql0b8fgkagtj0auf4vnt97.apps.googleusercontent.com',
+//     googleClientSecret: '40lp2Hf8wl4ILLMPNrv8q4GM',
+//     mongoURI: 'mongodb://lfreire80prod:da!170707prod@ds155243.mlab.com:55243/emaily-prod-lab',
+//     cookieKey: 'fsfwef3rt43t4hg6hrgherg45yhrfgdsgdftgret5tgtfes'
+// };
