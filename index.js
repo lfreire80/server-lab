5f98be4e2cafd8447879d7eb5a0805508aa14d52
Leonardo Freire <lfreire80@gmail.com>
2018-11-05 23:23:08 -0200

    oauth

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Works !!!!')
-});
+passport.use(new GoogleStrategy());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
