b60cdb5c680f40d702fa6dd8f2cd54c36b12c248
Leonardo Freire <lfreire80@gmail.com>
2018-11-09 15:26:21 -0300

    server side e client side dev proxy

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <body>
+          <a href="/auth/google">Login with Google</a>
+        </body>
       </div>
     );
   }
